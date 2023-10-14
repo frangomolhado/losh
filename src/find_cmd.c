161e66bf3eed0d07774ee87713cf18b3de8fc864
Lohan Yrvine <o.lohan.yrvine@gmail.com>
2023-10-14 10:19:35 -0300

    refactor(find_cmd): splitting 'find_all_cmd' function and some improvement in memory management

--- src/find_cmd.c
+++ src/find_cmd.c
@@ -1,53 +1,120 @@
+// necessary to use `strdup`
+#define _POSIX_C_SOURCE 200809L
+
 #include "find_cmd.h"
 
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 
-static char *join_paths(const char *root, const char *subdirs) {
-    size_t size = strlen(root) + 1 + strlen(subdirs) + 1;
+typedef struct {
+    char **paths;
+    size_t size;
+} PathsList;
+
+PathsList *alloc_paths_list(void) {
+    PathsList *pl = malloc(sizeof(PathsList));
+    pl->paths = NULL;
+    pl->size = 0;
+    return pl;
+}
+
+static char *join_paths(const char *root, const char *relative) {
+    size_t size = strlen(root) + 1 + strlen(relative) + 1;
     char *result = calloc(size, 0);
+    if (result == NULL) {
+        return NULL;
+    }
     strcat(result, root);
     strcat(result, "/");
-    strcat(result, subdirs);
+    strcat(result, relative);
     strcat(result, "\0");
     return result;
 }
 
-// FIXME: the bad use of realloc here can cause a dangling pointer in case of
-// lack of memory
-char *find_all_command(const char *command) {
-    char *result = NULL;
+static bool is_binary(const char *path) {
+    struct stat file_info;
+    if (stat(path, &file_info) == 0) {
+        return file_info.st_mode & S_IXUSR;
+    }
+
+    return false;
+}
+
+static bool add_path(PathsList *pl, const char *path) {
+    if (pl->paths == NULL) {
+        pl->paths = malloc(sizeof(char *));
+        if (pl->paths == NULL) {
+            return false;
+        }
+        pl->paths[pl->size++] = strdup(path);
+    } else {
+        char **tmp = realloc(pl->paths, sizeof(char *) * (pl->size + 1));
+        if (tmp == NULL) {
+            return false;
+        }
+        pl->paths = tmp;
+        pl->paths[pl->size++] = strdup(path);
+    }
+
+    return true;
+}
+
+static char *concatenate_paths(PathsList *pl, char c) {
+    // starter value = number of `c` that will be necessary
+    size_t size = pl->size - 1;
+    for (size_t i = 0; i < pl->size; i++) {
+        size += strlen(pl->paths[i]);
+    }
+
+    char *result = malloc(size + 1);
+    if (result == NULL) {
+        return NULL;
+    }
+
+    size_t offset = 0;
+    for (size_t i = 0; i < pl->size; i++) {
+        strcpy(result + offset, pl->paths[i]);
+        offset += strlen(pl->paths[i]);
+        result[offset++] = c;
+    }
+    // change last '\n' to '\0'
+    result[offset - 1] = '\0';
+
+    return result;
+}
+
+char *find_all_command(const char *cmd) {
+    PathsList *pl = alloc_paths_list();
+    if (pl == NULL) {
+        return NULL;
+    }
     const char *delim = ":";
     char default_path[] = DEFAULT_PATH;
     char *dir_path = strtok(default_path, delim);
     while (dir_path) {
-        char *absolute_command_path = join_paths(dir_path, command);
-        struct stat file_info;
-        if (stat(absolute_command_path, &file_info) == 0) {
-            if (file_info.st_mode & S_IXUSR) {
-                if (!result) {
-                    result = calloc(strlen(absolute_command_path), 0);
-                } else {
-                    result = realloc(result,
-                                     1 + strlen(result) +
-                                         strlen(absolute_command_path));
-                    strcat(result, "\n");
-                }
-                strcat(result, absolute_command_path);
+        char *cmd_path = join_paths(dir_path, cmd);
+        if (cmd_path == NULL) {
+            return NULL;
+        }
+
+        if (is_binary(cmd_path)) {
+            bool successfull = add_path(pl, cmd_path);
+            if (!successfull) {
+                return NULL;
             }
         }
-        free(absolute_command_path);
+
+        free(cmd_path);
         dir_path = strtok(NULL, delim);
     }
 
-    if (!result) {
-        return result;
+    if (pl->paths == NULL) {
+        return NULL;
     }
 
-    result = realloc(result, strlen(result) + 1);
-    strcat(result, "\0");
-    return result;
+    return concatenate_paths(pl, '\n');
 }
 
 char *find_command(const char *cmd) {
