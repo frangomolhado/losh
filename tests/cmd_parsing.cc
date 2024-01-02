0db682ac6a0807180dccac68a41c2367e2923d95
Lohan Yrvine <o.lohan.yrvine@gmail.com>
2024-01-02 18:00:18 -0300

    fix(parse test): fixing test model

--- tests/cmd_parsing.cc
+++ tests/cmd_parsing.cc
@@ -7,13 +7,14 @@ extern "C" {
 
 TEST(CommandParsingValidInput, BasicInput) {
     char input_mock[] = "echo banana\n";
-    Command *cmd = get_cmd(input_mock);
+    Command cmd;
+    init_cmd(&cmd);
+    parse_cmd(input_mock, &cmd);
 
-    EXPECT_NE(cmd, nullptr);
-    EXPECT_EQ(cmd->size, 3);
-    EXPECT_EQ(cmd->capacity, 8);
+    EXPECT_EQ(cmd.size, 3);
+    EXPECT_EQ(cmd.capacity, 8);
 
-    EXPECT_TRUE(strcmp(cmd->args[0], "echo") == 0);
-    EXPECT_TRUE(strcmp(cmd->args[1], "banana") == 0);
-    EXPECT_EQ(cmd->args[2], nullptr);
+    EXPECT_TRUE(strcmp(cmd.args[0], "echo") == 0);
+    EXPECT_TRUE(strcmp(cmd.args[1], "banana") == 0);
+    EXPECT_EQ(cmd.args[2], nullptr);
 }
